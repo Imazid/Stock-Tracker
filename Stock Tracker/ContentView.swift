8e8e6251d805d930c37f151c0c59d8e0f7691fa7
Imazid <153601920+Imazid@users.noreply.github.com>
2025-11-28 23:34:03 +1100

    Initial Commit

---
+++ Stock Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stock Tracker
+//
+//  Created by Ihtisham Mazid on 28/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stock Tracker/Stock_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stock_TrackerApp.swift
+//  Stock Tracker
+//
+//  Created by Ihtisham Mazid on 28/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct Stock_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
